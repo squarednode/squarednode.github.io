d41e086185ea2756dd4b21e6a3400c8ca977c01e
Chris Erickson <80534741+squarednode@users.noreply.github.com>
2021-04-05 15:11:11 -0300

    Testing php adding file
    Trying to add file with php

---
+++ FCC/head.php
@@ -0,0 +1,26 @@
+<title>Squared Node Web design and digital services</title>
+    <link
+      rel="shortcut icon"
+      type="image/jpg"
+      href="https://github.com/squarednode/image_repo/blob/main/logo_squarednode_icon.jpg?raw=true"
+    />
+    <meta
+      name="description"
+      content="Digital Assests for custom websites, engineering, and photo editing"
+    />
+    <meta
+      name="keywords"
+      content="web de, website, website design, 
+        website builder, website templates, web, web development, web page, web designer, 
+        engineer, engineering, mech engineer, mechanical engineering, autocad, solidworks, 
+        revit, mechanical design, drafting, design, 
+        photo editing, adobe, photoshop, photos, rendering, paintings, poster, custom, digital, products, for sale, hire"
+    />
+    <meta name="author" content="Squared Node" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale = 1.0, minimum-scale=1.0, maximum-scale=1.5"
+    />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="header.css" />
+    <link rel="stylesheet" href="footer.css" />
